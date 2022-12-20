c06285f73d6445d001ca3e56d00e30a37e2d23d3
Aniebiet Afia <leeboy4u2002@yahoo.com>
2022-12-20 01:39:14 -0800

    Convert to an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const number = Number(process.argv[2]);
 if (Number.isNaN(number)) {
-	console.log('Not a number');
-}else{
-	console.log('My number: ' + number);
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
 }
